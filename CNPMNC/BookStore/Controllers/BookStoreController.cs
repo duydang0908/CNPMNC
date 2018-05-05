3114c194477251619a7923500524a6a0d0be63a9
ncdanh19 <33634037+ncdanh19@users.noreply.github.com>
2018-05-05 14:23:42 +0700

    up all

---
+++ CNPMNC/BookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        dbQLBansachDataContext data = new dbQLBansachDataContext();
+
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+
+        public ActionResult SpTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes
+                       where s.Masach == id
+                       select s;
+            return View(sach.Single());
+        }
+
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+
+        // GET: BookStore
+        //public ActionResult Index(int? page)
+        //{
+        //    int pageSize = 5;
+        //    int pageNum = (page ?? 1);
+        //    var sachmoi = Laysachmoi(15);
+        //    return View(sachmoi.ToPagedList(pageNum, pageSize));
+
+
+        //    //var sachmoi = Laysachmoi(5);
+        //    //return View(sachmoi);
+        //}
+
+        public ActionResult Index(int? page, string searchString)
+        {
+            var sp = from e in data.SACHes select e;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sp = sp.Where(s => s.Tensach.Contains(searchString));
+            }
+            ViewBag.SeachString = searchString;
+            //tạo bien so trang
+            int pageNumber = (page ?? 1);
+            //tao bien quy dinh so san pham tren moi trang
+            int pageSize = 6;
+            return View(sp.ToList().OrderBy(n => n.Masach).ToPagedList(pageNumber, pageSize));
+            //return View(db.SAN_PHAMs.ToList());
+        }
+
+
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult Nhaxuatban()
+        {
+            var nxb = from cd in data.NHAXUATBANs select cd;
+            return PartialView(nxb);
+        }
+
+    }
+}
---
+++ CNPMNC/BookStore/Controllers/NguoiDungController.cs
@@ -0,0 +1,95 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbQLBansachDataContext db = new dbQLBansachDataContext();
+
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var nhaplaimatkhau = collection["nhaplaimatkhau"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["Ngaysinh"]);
+            if (String.IsNullOrEmpty(hoten))
+                ViewData["Loi1"] = "Ho ten khach hang khong duoc de trong";
+            else if (String.IsNullOrEmpty(tendn))
+                ViewData["Loi2"] = "Phai nhap ten dang nhap";
+            else if (String.IsNullOrEmpty(matkhau))
+                ViewData["Loi3"] = "Phai nhap mat khau";
+            else if (String.IsNullOrEmpty(nhaplaimatkhau))
+                ViewData["Loi4"] = "Mat khau nhap lai khong bi de trong";
+            else if (string.IsNullOrEmpty(email))
+                ViewData["Loi5"] = "Email khong duoc bo trong";
+            else if (string.IsNullOrEmpty(dienthoai))
+                ViewData["Loi6"] = "Khong duoc bo trong sdt";
+            else
+            {
+                kh.HoTenKH = hoten;
+                kh.TenDN = tendn;
+                kh.Matkhau = matkhau;
+                kh.Email = email;
+                kh.DiachiKH = diachi;
+                kh.DienthoaiKH = dienthoai;
+                kh.Ngaysinh = DateTime.Parse(ngaysinh);
+                db.KHACHHANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("Dangnhap");
+            }
+            return this.Dangky();
+        }
+
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection collection)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+                ViewData["Loi1"] = "Phai nhap ten dang nhap";
+            else if (String.IsNullOrEmpty(matkhau))
+                ViewData["Loi2"] = "Phai nhap mat khau";
+            else
+            {
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.TenDN == tendn && n.Matkhau == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.ThongBao = "Dang nhap thanh cong";
+                    Session["Taikhoan"] = kh;
+                }
+                else
+                    ViewBag.ThongBao = "Ten dang nhap hoac mat khau khong dung";
+            }
+            return View();
+        }
+    }
+}
